e2bfcef40c160e8e7551c683046417433cf81f09
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-23 11:11:20 -0400

    Agregar ejemplos de funciones lambda en un nuevo archivo

---
+++ funciones_lambda.py
@@ -0,0 +1,14 @@
+# FUnciones Lambda 
+# Sintaxis lambda argumentos: expresion se usa para codigos compactos
+
+print("Funciones Lambda")
+
+def cuadrado(x):
+    return x**2
+
+print(cuadrado(5)) # 25
+
+# Usando Lambda
+cuadrado_lambda = lambda x: x**2
+print(cuadrado_lambda(5)) # 25
+
