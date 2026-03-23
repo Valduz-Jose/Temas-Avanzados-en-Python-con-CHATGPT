d8fca1a488fb37302889f1fa3cd52d21a6befb04
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-23 11:18:26 -0400

    Agregar ejemplos de uso de reduce en el archivo map_y_filter.py

--- map_y_filter.py
+++ map_y_filter.py
@@ -10,3 +10,8 @@ print(cuadrados) # Output: [1, 4, 9, 16, 25]
 
 pares = list(filter(lambda x: x % 2 == 0, numeros)) #el primer parametro es la funcion lambda que devuelve True si el numero es par, y el segundo parametro es la lista numeros
 print(pares) # Output: [2, 4]
+
+#reduce permite reducir un iterable a un solo valor aplicando una función acumulativa a los elementos del iterable.
+from functools import reduce
+suma = reduce(lambda x, y: x + y, numeros) #el primer parametro es la funcion lambda que suma dos elementos, y el segundo parametro es la lista numeros
+print(suma) # Output: 15
