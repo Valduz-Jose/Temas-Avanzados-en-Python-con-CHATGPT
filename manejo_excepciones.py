f3d1aef6d7cdd9ccccc5d4f98bdfe7cb23feb1c3
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-23 12:00:51 -0400

    Refactor manejo de excepciones en la función dividir para mejorar la claridad y manejo de errores

--- manejo_excepciones.py
+++ manejo_excepciones.py
@@ -6,15 +6,23 @@ print("*** Manejo de Excepciones ***")
 
 def dividir(numerador, denominador):
     try:
+        # Revisamos si el denominador es igual a 0
+        if denominador == 0:
+            raise Exception("El denominador no puede ser cero.")
         resultado = numerador/denominador
-        return resultado
-    except ZeroDivisionError:
-        print("Error: No se puede dividir por cero.")
-    except TypeError:
-        print("Error: Tipo de dato no válido para la división.")
+        # return resultado
+        print(f"El resultado de la división es: {resultado}")
+    # except ZeroDivisionError:
+    #     print("Error: No se puede dividir por cero.")
+    # except TypeError:
+    #     print("Error: Tipo de dato no válido para la división.")
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        
+    else:
+        print("La división se realizó correctamente.")
+        # Se ejecuta si no se produce ninguna excepción
 
-    # except Exception as e:
-    #     print(f"Error inesperado: {e}")
     # Este es mas generico para los errores que no se han manejado especificamente
     finally:
         print("Esta sección se ejecuta siempre, haya o no una excepción.")
